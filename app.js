8f9f5a171d4a2d239a864bb3a06e758ca481c249
Jeofrey Ditsela <64729609+ditselajeofrey@users.noreply.github.com>
2022-09-06 01:20:45 +0200

    improved awesome js with increase content

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ window.onload=function(){
     for(var i=0; i<10; i++){
         console.log(i);
     }
-    console.lo('you for loop demo is awesome');
+    console.lo('your for-loop demo is awesome, this is practice session to see how i can contribute something. You can always deleteit because i did not have a dummy account. Dont take it serious hey');
 }
