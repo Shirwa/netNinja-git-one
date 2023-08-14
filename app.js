032ad77ab632f7524dcf4db1e866c8917541ca5a
Shirwa <yshirwa24@gmail.com>
2023-08-15 02:25:04 +0300

    netninga

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-window.onload=function(){
-    for(var i=0; i<10; i++){
-        console.log(i);
-    }
-    console.lo('your for-loop demo is awesome, this is practice session to see how i can contribute something. You can always deleteit because i did not have a dummy account. Dont take it serious hey');
-}
+window.onload = function () {
+  for (var i = 0; i < 10; i++) {
+    console.log(i);
+  }
+  console.lo(
+    "your for-loop demo is awesome, this is practice session to see how i can contribute something. You can always deleteit because i did not have a dummy account. Dont take it serious hey"
+  );
+  console.log(`you are awesome my friend`);
+};
